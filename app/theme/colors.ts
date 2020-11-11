a8a73239d1dbf4c90e7013d9412a9bc8681cfcec
MadhavNasit <40732694+MadhavNasit@users.noreply.github.com>
2020-11-11 16:07:12 +0530

    first commit

---
+++ app/theme/colors.ts
@@ -0,0 +1,14 @@
+export const color = {
+  primary: 'rgb(44 ,89 ,112)',
+  white: "#fff",
+  background: '#fff',
+  headerText: '#fff',
+  buttonText: '#fff',
+  black: "#000",
+  gray: "#d3d3d3",
+  errorText: "#FF0000",
+  formInputBorder: "#787878",
+  text: "#000",
+  placeHolder: "#d3d3d3",
+  button: '#4c00ff'
+}
